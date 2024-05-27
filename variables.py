82756891a5abbf976316d30ec8cb98ba83d2430e
Ipsita parida <130665003+ipsitaparida22@users.noreply.github.com>
2024-05-27 18:44:08 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+#Create two variables. One to store your birth year and another one to store current year. Now calculate your age using these two variables
+
+birth_year = 2004
+current_year = 2024
+age = current_year-birth_year
+print(age)
+
+#Store your first, middle and last name in three different variables and then print your full name using these variables
+
+first_name = "Ipsita"
+last_name = "parida"
+print("my full name is : " + first_name +" " + last_name)
