6fc2da06621b24cacd052720d4684201c8706a85
Ipsita parida <130665003+ipsitaparida22@users.noreply.github.com>
2024-05-29 21:32:02 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+population = {'china': 143, 'india': 136, 'usa': 32, 'pakistan': 21}
+def add():
+    country=input("Enter country name to add:")
+    country=country.lower()
+    if country in population:
+        print("Country already exist in our dataset. Terminating")
+        return
+    p=input(f"Enter population for {country}")
+    p=float(p)
+    population[country]=p # Adds new key value pair to dictionary
+    print_all()
+
+    
